61d6e0c679d2cb1c589cdaea09ad64a4138ecfe0
fu xuanyu <no10pc@gmail.com>
2012-03-23 14:28:54 +0800

    test

---
+++ helloWorld/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace helloWorld
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
